a6c0c78abe798d5c8d8d40c45d4ba39096e08420
ArnaldOzPy <156711582+ArnaldOzPy@users.noreply.github.com>
2025-07-23 19:28:48 -0300

    Add files via upload

---
+++ workers/decompress-worker.js
@@ -0,0 +1,18 @@
+import { inverse6D } from '../bwt-rle.js';
+import { decompressHS6D } from '../hyper-huffman.js';
+
+self.onmessage = (e) => {
+try {
+const compressedData = e.data;
+const huffmanDecompressed = decompressHS6D(compressedData);
+const originalData = inverse6D(huffmanDecompressed);
+
+self.postMessage({  
+  decompressed: originalData,  
+  compressedSize: compressedData.length  
+}, [originalData.buffer]);
+
+} catch (error) {
+self.postMessage({ error: "Error en descompresión: " + error.message });
+}
+};
